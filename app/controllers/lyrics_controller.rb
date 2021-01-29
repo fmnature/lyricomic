c5af6329e3b02f233e375e28b129325fa825f79b
fmnature <sekaowa75.uhh@gmail.com>
2021-01-29 14:07:41 +0900

    ログインログアウト機能

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,5 +1,6 @@
 class LyricsController < ApplicationController
   before_action :set_lyric, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @lyrics = Lyric.all.order("created_at DESC")
@@ -49,5 +50,11 @@ class LyricsController < ApplicationController
 
   def set_lyric
     @lyric = Lyric.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
