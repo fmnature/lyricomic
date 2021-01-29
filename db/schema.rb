f65a36ca9477fe8c075bd02041393ead9b8ed9af
fmnature <sekaowa75.uhh@gmail.com>
2021-01-29 14:16:01 +0900

    lyricsテーブルにuser_idの紐付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_171041) do
+ActiveRecord::Schema.define(version: 2021_01_29_051420) do
 
   create_table "lyrics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "word", null: false
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
