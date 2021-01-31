8c6fb2965a0c680bee87962fe38bfa980ed75202
fmnature <sekaowa75.uhh@gmail.com>
2021-02-01 01:01:33 +0900

    レイアウト編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,10 @@ class CommentsController < ApplicationController
     redirect_to lyric_path(comment.lyric.id)
   end
 
+  def edit
+    
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, lyric_id: params[:lyric_id])
