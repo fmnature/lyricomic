77087cb6f098cd5fad1c71cd06f1b1ceec361cdd
fmnature <sekaowa75.uhh@gmail.com>
2021-02-02 18:55:17 +0900

    フォロー機能雛形

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id) if user_signed_in?
+
     user = User.find(params[:id])
     @lyrics = Lyric.all
     @id = user.id
@@ -8,6 +9,8 @@ class UsersController < ApplicationController
     @image = user.image
     @profile = user.profile
     @lyrics = user.lyrics.order("created_at DESC")
+
+
   end
 
   def edit
