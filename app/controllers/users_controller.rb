d4683b74cc3596d0b62724ca9007a0279699392b
fmnature <sekaowa75.uhh@gmail.com>
2021-02-02 17:13:25 +0900

    レイアウト編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id) if user_signed_in?
     user = User.find(params[:id])
+    @lyrics = Lyric.all
     @id = user.id
     @nickname = user.nickname
     @image = user.image
