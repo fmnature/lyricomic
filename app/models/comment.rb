c832695bf3c5d28cb13f12271e7340d3ce9a8448
fmnature <sekaowa75.uhh@gmail.com>
2021-02-04 14:52:05 +0900

    コメント通知機能

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :lyric
   belongs_to :user
+  has_many :notifications, dependent: :destroy
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,7 @@ class Notification < ApplicationRecord
   default_scope -> {order(created_at: :desc)}
 
   belongs_to :lyric, optional: true
+  belongs_to :comment, optional: true
 
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id', optional: true
   belongs_to :sended, class_name: 'User', foreign_key: 'sended_id', optional: true
