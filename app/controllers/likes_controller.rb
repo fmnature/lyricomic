03579b3068280d815e1d6908aeffb6c3ae41a25d
fmnature <sekaowa75.uhh@gmail.com>
2021-02-04 11:33:43 +0900

    通知機能雛形

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,8 @@
 class LikesController < ApplicationController
   def create
     @like = current_user.likes.create(lyric_id: params[:lyric_id])
+    lyric = Lyric.find(params[:lyric_id])
+    lyric.create_notification_like!(current_user)
     redirect_back(fallback_location: root_path)
   end
 
