56d6935cc5bcaab1faf5b621d8bba9d92e971041
fmnature <sekaowa75.uhh@gmail.com>
2021-02-09 19:31:48 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,15 +3,13 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.create(lyric_id: params[:lyric_id])
-    # lyric = Lyric.find(params[:lyric_id])
-    # lyric.create_notification_like!(current_user)
-    # redirect_back(fallback_location: root_path)
+    lyric = Lyric.find(params[:lyric_id])
+    lyric.create_notification_like!(current_user)
   end
 
   def destroy
     @like = Like.find_by(lyric_id: params[:lyric_id], user_id: current_user.id)
     @like.destroy
-    # redirect_back(fallback_location: root_path)
   end
 
   def set_lyric
