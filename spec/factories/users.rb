96be5c3dbcbeb214176ae1ab623ba177236e5f70
fmnature <sekaowa75.uhh@gmail.com>
2021-02-15 16:54:41 +0900

    テストコード設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.last_name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    profile               {'魚'}
+    category_id           {2}
+  end
+end
