17cc40689069695517b22e8248dc032b06eff79a
fmnature <sekaowa75.uhh@gmail.com>
2021-02-18 10:14:11 +0900

    編集

---
+++ app/javascript/packs/scroll.js
@@ -0,0 +1,16 @@
+// $(document).on('scroll', function() {
+//   scrollHeight = $(document).height();
+//   scrollPosition = $(window).height() + $(window).scrollTop();
+//   if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.1) {
+//           var jscrollOption = {
+//               // callback: true,
+//               loadingHtml: '<div class="loading"><img src="/assets/Preloader_1.gif" alt="Loading" /> Loading...</div>', 
+//               autoTrigger: true, 
+//               padding: 20,
+//               nextSelector: 'span.next:last a', 
+//               contentSelector:' .ex-con'
+//           }
+//           $('.jscroll').jscroll();
+
+//   }
+// });
