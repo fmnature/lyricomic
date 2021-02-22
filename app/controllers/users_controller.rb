087da4d63765427868e5251ba8cadd329fe9d8d5
fmnature <sekaowa75.uhh@gmail.com>
2021-02-22 17:03:04 +0900

    リリック・いいね・コメント見出し作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @lyrics = Lyric.all
+    @user_likes = @user.likes
     @relationship = current_user.relationships.find_by(follow_id: @user.id)  
     @set_relationship = current_user.relationships.new
 
