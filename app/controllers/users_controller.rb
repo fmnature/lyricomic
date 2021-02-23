761a6ab9428f6ef5d152024e66948410e4693fce
fmnature <sekaowa75.uhh@gmail.com>
2021-02-23 17:28:32 +0900

    編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @lyrics = Lyric.all
-    @user_likes = @user.likes
     @relationship = current_user.relationships.find_by(follow_id: @user.id)  
     @set_relationship = current_user.relationships.new
 
